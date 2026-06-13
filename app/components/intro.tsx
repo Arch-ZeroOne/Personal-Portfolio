f4541751494511b82b3ac072a4f659bf22c008a2
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-06-13 20:16:34 +0800

    initial commit

---
+++ app/components/intro.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <section className="flex flex-col gap-10 w-[80%] mr-auto ml-auto">
+      <div>
+        Hello! My name is <span>Windyl Monton </span>, and I am currently a
+        third-year Bachelor of Science in Information Technology (BSIT) student
+        with a strong interest in web development. As an aspiring web developer,
+        I am passionate about creating functional, responsive, and user-friendly
+        web applications that provide meaningful experiences to users. I
+        continuously seek opportunities to expand my knowledge and improve my
+        technical skills through academic projects, self-learning, and hands-on
+        practice.
+      </div>
+      <div>
+        I am eager to grow both personally and professionally in the field of
+        technology by exploring modern web development tools and best practices.
+        With determination, adaptability, and a willingness to learn, I aim to
+        become a competent web developer capable of contributing innovative
+        solutions to real-world challenges. I believe that every project and
+        learning experience brings me one step closer to achieving my goals in
+        the ever-evolving world of information technology.
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
