7bdc1581f4d37a427c8a5940bc517695db59ae41
Arch-ZeroOne <windyl1.monton.lvhs@gmail.com>
2026-06-21 17:32:27 +0800

    Updated the contect me section

--- app/components/book.tsx
+++ app/components/book.tsx
@@ -1,40 +1,52 @@
 import React from "react";
-
+import { Space_Grotesk } from "next/font/google";
+const space_grotesk = Space_Grotesk({
+  weight: "400",
+  subsets: ["latin"],
+});
 const Book = () => {
   return (
-    <section className="flex items-center  gap-6 w-[50%] mr-auto ml-auto mt-15">
-      <div className="text-center lg:text-left justify-center items-center">
-        <h1 className="text-3xl font-bold">Got A Project in Mind?</h1>
-        <p className="py-6">
-          Book a consultation for your web development project. Complete this
-          form with your project details, goals, and requirements, and we'll get
-          in touch to discuss the best solution for your website.
-        </p>
-      </div>
-      <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
-        <div className="card-body">
-          <fieldset className="fieldset">
-            <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
-            <label className="label">Project Type</label>
-            <select defaultValue="Pick a color" className="select">
-              <option disabled={true}>Project Type</option>
-              <option>Landing Page</option>
-              <option>Fullstack Application</option>
-              <option>IOT and Web Project</option>
-              <option>Console Based Project</option>
-            </select>
-            <label className="label">Appointment Date</label>
-            <input type="datetime-local" className="input" />
+    <div className={space_grotesk.className}>
+      <section className="flex items-center  gap-6 w-[50%] mr-auto ml-auto mt-15">
+        <div className="flex flex-col items-center">
+          <div className="text-center lg:text-left justify-center items-center">
+            <h1 className="text-3xl font-bold text-center">
+              Got A Project in Mind?
+            </h1>
+            <p className="py-6 text-center">
+              Book a consultation for your web development project. Complete
+              this form with your project details, goals, and requirements, and
+              we'll get in touch to discuss the best solution for your website.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-md shrink-0 shadow-2xl">
+            <div className="card-body flex flex-col items-center w-full">
+              <fieldset className="fieldset w-full flex  flex-col items-center">
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" />
+                <label className="label">Project Type</label>
+                <select defaultValue="Pick a color" className="select">
+                  <option disabled={true}>Project Type</option>
+                  <option>Landing Page</option>
+                  <option>Fullstack Application</option>
+                  <option>IOT and Web Project</option>
+                  <option>Console Based Project</option>
+                </select>
+                <label className="label">Appointment Date</label>
+                <input type="datetime-local" className="input" />
 
-            <label className="label">Message(Optional)</label>
-            <textarea className="textarea" placeholder="Message"></textarea>
+                <label className="label">Message(Optional)</label>
+                <textarea className="textarea" placeholder="Message"></textarea>
 
-            <button className="btn btn-neutral mt-4">Book Appointment</button>
-          </fieldset>
+                <button className="btn btn-neutral mt-4 self-center">
+                  Book Appointment
+                </button>
+              </fieldset>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
